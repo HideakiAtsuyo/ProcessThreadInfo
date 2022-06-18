0d17efb6cbd382d1df28b38d696d51f6740b149a
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2022-06-18 14:50:46 -0700

    SRC

---
+++ ProcessThreadInfo/ProcessThreadInfoForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ProcessThreadInfo
+{
+    partial class ProcessThreadInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonWalk = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listViewCallStack = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // buttonWalk
+            // 
+            this.buttonWalk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonWalk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonWalk.Location = new System.Drawing.Point(315, 29);
+            this.buttonWalk.Name = "buttonWalk";
+            this.buttonWalk.Size = new System.Drawing.Size(83, 23);
+            this.buttonWalk.TabIndex = 16;
+            this.buttonWalk.Text = "Get Threads";
+            this.buttonWalk.UseVisualStyleBackColor = true;
+            this.buttonWalk.Click += new System.EventHandler(this.buttonWalk_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.BackColor = System.Drawing.Color.Black;
+            this.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.TextBox1.Location = new System.Drawing.Point(84, 29);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(217, 20);
+            this.TextBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Process ID:";
+            // 
+            // listViewCallStack
+            // 
+            this.listViewCallStack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewCallStack.BackColor = System.Drawing.Color.Black;
+            this.listViewCallStack.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewCallStack.ForeColor = System.Drawing.Color.Lime;
+            this.listViewCallStack.FullRowSelect = true;
+            this.listViewCallStack.HideSelection = false;
+            this.listViewCallStack.Location = new System.Drawing.Point(15, 58);
+            this.listViewCallStack.Name = "listViewCallStack";
+            this.listViewCallStack.ShowItemToolTips = true;
+            this.listViewCallStack.Size = new System.Drawing.Size(383, 360);
+            this.listViewCallStack.TabIndex = 13;
+            this.listViewCallStack.UseCompatibleStateImageBehavior = false;
+            this.listViewCallStack.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "TID";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Symbol Name";
+            this.columnHeader4.Width = 220;
+            // 
+            // ProcessThreadInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(415, 446);
+            this.Controls.Add(this.buttonWalk);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listViewCallStack);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProcessThreadInfoForm";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProcessThreadInfoForm | github.com/HideakiAtsuyo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonWalk;
+        internal System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewCallStack;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ ProcessThreadInfo/ProcessThreadInfoForm.cs
@@ -0,0 +1,74 @@
+﻿using ProcessHacker;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ProcessThreadInfo
+{
+    public partial class ProcessThreadInfoForm : Form
+    {
+        private static ThreadProvider _threadP = null;
+        public ProcessThreadInfoForm()
+        {
+            InitializeComponent();
+            this.MouseDown += ProcessThreadInfo_MouseDown;
+        }
+
+        #region Form Event
+
+        private void ProcessThreadInfo_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                NativeStuff.ReleaseCapture();
+                NativeStuff.SendMessage(Handle, NativeStuff.WM_NCLBUTTONDOWN, NativeStuff.HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        private void buttonWalk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process processEx = Process.GetProcessById(int.Parse(TextBox1.Text));
+
+                listViewCallStack.Items.Clear();
+
+                _threadP = new ThreadProvider(processEx.Id);
+                _threadP.LoadKernelSymbols();
+                _threadP.Run();
+
+                _threadP.BeforeUpdate += _threadP_BeforeUpdate;
+                _threadP.ResolveSybol += _threadP_ResolveSybol;
+                _threadP.Error += _threadP_Error;
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Process ID ?");
+            }
+        }
+
+        #region Thread Events
+
+        private void _threadP_Error(Exception ex) => MessageBox.Show(ex.ToString());
+
+        private void _threadP_ResolveSybol(object sender, ThreadProvider.ResolveSybolData e)
+        {
+            if (listViewCallStack.InvokeRequired)
+            {
+                //MessageBox.Show("OK");
+                listViewCallStack.Invoke((MethodInvoker)delegate ()
+                {
+                    ListViewItem itemN = new ListViewItem();
+                    itemN.Text = e.TID.ToString();
+                    itemN.SubItems.Add(e.ResolveInfo.Symbol);
+                    listViewCallStack.Items.Add(itemN);
+                    //listViewCallStack.EnsureVisible(listViewCallStack.Items.Count - 1);
+                });
+            }
+        }
+
+        private void _threadP_BeforeUpdate() => listViewCallStack.Items.Clear();
+        #endregion
+    }
+}
